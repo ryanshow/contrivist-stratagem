95a1ee13e03e2c75f36d9ace34fc83c7c815aa46
Ryan Showalter <ryan@perkville.com>
2014-02-09 00:35:43 -0800

    Get the basic project structure going

---
+++ src/main.cpp
@@ -0,0 +1,41 @@
+#include <GLFW/glfw3.h>
+#include <format.h>
+
+int main(int argc, char **argv)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(
+            800, 600,
+            fmt::c_str(fmt::Format("Contravist Stratagem v{0}.{1}") << 0 << 1),
+            nullptr, nullptr);
+
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
